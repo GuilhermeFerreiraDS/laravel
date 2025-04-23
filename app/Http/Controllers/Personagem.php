8eedbcfc4d0ea73e874545af1fef960b3df670f9
GuilhermeFerreiraDS <guilhermecristian906@gmail.com>
2025-04-23 18:24:28 -0300

    listar-personagem

--- app/Http/Controllers/Personagem.php
+++ app/Http/Controllers/Personagem.php
@@ -18,6 +18,7 @@ class Personagem extends Controller
 
     
     function listarPersonagem(){
-        return view('listar-personagem');
+        $personagem = Personagens::all()->toArray()->paginate(3);
+        return view('listar-personagem', ['personagem' => $personagem]);
     }
 }
